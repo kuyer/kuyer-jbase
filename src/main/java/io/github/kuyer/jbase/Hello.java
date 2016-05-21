be2de1b60c31ddbacf552fd634786fd03f3c2e5f
rory.zhang <coolrl@126.com>
2016-05-21 11:31:37 +0800

    Maven化项目，增加Hello类

---
+++ src/main/java/io/github/kuyer/jbase/Hello.java
@@ -0,0 +1,35 @@
+package io.github.kuyer.jbase;
+
+import java.util.Scanner;
+
+/**
+ * Java的首个程序，和世界打个招呼，说句话吧
+ * @author Rory.Zhang
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello, world!");
+		
+		System.out.println("what is your name?");
+		// Scanner扫描器，从System.in中扫描信息
+		Scanner console = new Scanner(System.in);
+		while(console.hasNext()) {
+			// 读取System.in中一行信息
+			String name = console.nextLine().trim();
+			if(name.equals("")) {
+				continue;
+			}
+			if(name.equals("exit") || name.equals("quit")) {
+				System.out.println("byebye!");
+				break;
+			}
+			String message = String.format("hello, %s!", name);
+			System.out.println(message);
+			
+			System.out.println("what is your name?");
+		}
+		console.close();
+	}
+
+}
