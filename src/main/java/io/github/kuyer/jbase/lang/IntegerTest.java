d307de4893265de70374065d95bba95ea9687c27
rory.zhang <coolrl@126.com>
2017-06-29 20:23:12 +0800

    for test

---
+++ src/main/java/io/github/kuyer/jbase/lang/IntegerTest.java
@@ -0,0 +1,11 @@
+package io.github.kuyer.jbase.lang;
+
+public class IntegerTest {
+
+	public static void main(String[] args) {
+		Integer i1 = 134;
+		Integer i2 = 134;
+		System.out.println(i1==i2);
+	}
+
+}
